627bbf8ecba56842510ccb2e0462505e10d78432
safiracita <safiracitak@gmail.com>
2023-12-28 13:56:35 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+data = [{
+    'img1': 'assest/rony-saf.jpg',
+    'img2': 'assest/rony-saf.jpg',
+    'title': 'web E-commerce',
+    'subtitle': 'Zenit',
+    'desc': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, reiciendis architecto numquam consectetur fugiat eaque.'
+}, {
+
+    'img1': 'assest/rony-saf.jpg',
+    'img2': 'assest/rony-saf.jpg',
+    'title': 'web E-commerce',
+    'subtitle': 'The Gallery',
+    'desc': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, reiciendis architecto numquam consectetur fugiat eaque.'
+
+}]
+var container = document.querySelector('.container-project')
+
+data.forEach(Element => {
+    container.innerHTML +=
+    `<div class="project">
+    <div class="img-project">
+        <img src="`+element.img1+`" alt="">
+        <img src="${element.img2}" alt="">
+    </div>
+    <div class="desc-project">
+        <h1>${element.title}</h1>
+        <h2>${element.subtitle}</h2>
+        <p>${element.desc}</p>
+    </div>
+    <button class="read-more">Read More</button>
+</div>`
+});
